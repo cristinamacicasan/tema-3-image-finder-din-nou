54e3442678b19e7f67dba9749055414184703ef0
cristinamacicasan <cmacicasan@gmail.com>
2024-04-13 10:17:34 +0300

    update app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import FetchImages from "./FetchImages";
+import ImageGalleryItem from "./ImageGalleryItem";
+import Searchbar from "./Searchbar";
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
+       style={{
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column', 
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
+      
     >
-      React homework template
+      <Searchbar /> 
+      <div style={{ marginTop: 20 }}></div>
+      <FetchImages />
+      <ImageGalleryItem/>
+
+      
     </div>
   );
 };
